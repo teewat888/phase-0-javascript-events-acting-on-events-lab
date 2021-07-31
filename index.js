20b1d23772daba639c2477b7a4eb05d4f5e43899
teerawat prarom <teewat@yahoo.com>
2021-08-01 00:06:32 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+document.addEventListener('keydown',function(e){
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft(){
+    const leftNumber = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumber,10);
+    if (left > 0){
+        dodger.style.left = `${left-1}px`;
+    }
+}
+function moveDodgerRight(){
+    const leftNumber = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumber,10);
+    if (left < 360){
+        dodger.style.left = `${left+1}px`;
+    }
+
+}
